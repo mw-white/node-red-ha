511ca811f859c08e32ced8558b5b09486b3dc207
Matt White <m.w.white@gmail.com>
2014-10-29 21:47:17 -0600

    Move request into helper function

--- isy/isy.js
+++ isy/isy.js
@@ -10,6 +10,7 @@
 module.exports = function(RED) {
   "use strict";
   var http = require('http'); // need http to talk to the ISY
+  var xml2js = require('xml2js');
 
   /*
    * Common ISY controller configuration - need host/port and 
@@ -34,6 +35,48 @@ module.exports = function(RED) {
     }
   });
 
+  function ISYREST(controller, path, callback) {
+    var options = {
+      hostname: controller.host,
+      port: controller.port,
+      method: 'GET',
+      auth: controller.credentials.uname + ":" +
+            controller.credentials.passwd,
+      path: path
+    }
+    var req = http.request(options, function(res) {
+      var result = "";
+      var done = false;
+
+      res.setEncoding('utf8');
+
+      res.on('end', function() {
+        if (done) {
+          return;
+        }
+        done = true;
+
+        xml2js.parseString(result, function(err, json) {
+          callback(err, json);
+        });
+      });
+
+      res.on('error', function(err) {
+        if (done) {
+          return;
+        }
+        done = true;
+
+        callback(err);
+      });
+
+      res.on('data', function(frag) {
+        result += frag;
+      });
+    });
+    req.end();
+  }
+
   /*
    * ISYSend - given a node id (either a deviceid like "00 11 22 1"
    * or a integer scene id), a controller config, and a command
@@ -44,15 +87,13 @@ module.exports = function(RED) {
   function ISYSend(config) {
     RED.nodes.createNode(this,config);
 
+    var node = this;
+
     this.controller = RED.nodes.getNode(config.controller);
     this.nodeid = config.node;
     this.command = config.command;
     if (this.controller) {
       this.on("input",function(msg) {
-        var url = "http://" + this.controller.credentials.uname + ":" + 
-            this.controller.credentials.passwd + "@" + this.controller.host + 
-            (this.controller.port==80?"":":" + this.controller.port) + 
-            "/rest/";
         var nid = this.nodeid;
         var cmd = this.command;
         if (msg.node) {
@@ -62,17 +103,16 @@ module.exports = function(RED) {
           cmd = msg.command;
         }
         if (nid && cmd) {
-          var options = {
-            hostname: this.controller.host,
-            port: this.controller.port,
-            method: 'GET',
-            auth: this.controller.credentials.uname + ":" +
-                  this.controller.credentials.passwd,
-            path: "/rest/nodes/" + encodeURIComponent(nid) + "/cmd/" + cmd
-          }
-          var req = http.request(options, function(res) {
+          var path="/rest/nodes/" + encodeURIComponent(nid) + "/cmd/" + cmd;
+
+          ISYREST(this.controller,path,function(err,json) {
+            if (!err) { 
+              msg.ret = json;
+            } else {
+              msg.payload = err;
+            }
+            node.send(msg);
           });
-          req.end();
         }
       });
     }
